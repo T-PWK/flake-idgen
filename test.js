c6fa49b6c5026621092615a117ac072c050b35f2
Tom <pawlaktom@gmail.com>
2014-06-02 14:52:18 +0100

    Tests added

--- test.js
+++ test.js
@@ -1,1 +1,24 @@
-var assert = require('assert');
+(function () {
+    "use strict";
+
+    var assert  = require('assert'),
+        format  = require('biguint-format'),
+        FlakeId = require('./flake-id-gen'),
+        idGen1  = new FlakeId();
+
+    test(idGen1);
+
+    function test(generator) {
+        var ids = new Array(1000), i;
+
+        for(i = 0; i < ids.length; i++) {
+            ids[i] = format(generator.next(), 'dec');
+        }
+
+        for(i = 0; i < ids.length - 1; i++) {
+            assert.notEqual(ids[i], ids[i+1]);      // Two sibling ids are not equal
+            assert.ok(ids[i] < ids[i+1]);           // Each id is greater than an id generated before
+        }
+    }
+
+}());
