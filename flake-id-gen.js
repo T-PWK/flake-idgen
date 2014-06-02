ae95482b22ebf5b9df7dfdb7a1cd7be8a2c92b87
Tom <pawlaktom@gmail.com>
2014-06-02 14:52:03 +0100

    Strict mode

--- flake-id-gen.js
+++ flake-id-gen.js
@@ -1,68 +1,81 @@
 /**
  * Flake ID generator yields k-ordered, conflict-free ids in a distributed environment.
  */
-var FlakeId = module.exports = function (options) {
-	this.options = options || {};
-	
-	// Set generator id from 'id' option or combination of 'datacenter' and 'worker'
-	if (typeof this.options.id !== 'undefined') {
-		this.id = this.options.id & 0x3FF;
-	} else {
-		this.id = ((this.options.datacenter || 0) & 0x1F) << 5 | ((this.options.worker || 0) & 0x1F)
-	}
-	this.id <<= 12;  // id generator identifier - will not change while generating ids
-	this.epoch = +this.options.epoch || 0;
-	this.seq = 0;
-	this.lastTime = 0;
-	this.overflow = false;
-};
+(function () {
+    "use strict";
 
-FlakeId.POW10 = Math.pow(2, 10); // 2 ^ 10
-FlakeId.POW26 = Math.pow(2, 26); // 2 ^ 26
+    var FlakeId = module.exports = function (options) {
+        this.options = options || {};
 
-FlakeId.prototype = {
-	next: function (cb) {
-		var id = new Buffer(8), time = Date.now() - this.epoch;
-		id.fill(0);
+        // Set generator id from 'id' option or combination of 'datacenter' and 'worker'
+        if (typeof this.options.id !== 'undefined') {
+            this.id = this.options.id & 0x3FF;
+        } else {
+            this.id = ((this.options.datacenter || 0) & 0x1F) << 5 | ((this.options.worker || 0) & 0x1F)
+        }
+        this.id <<= 12;  // id generator identifier - will not change while generating ids
+        this.epoch = +this.options.epoch || 0;
+        this.seq = 0;
+        this.lastTime = 0;
+        this.overflow = false;
+    };
 
-		// Generates id in the same millisecond as the previous id
-		if(time === this.lastTime) {
+    FlakeId.POW10 = Math.pow(2, 10); // 2 ^ 10
+    FlakeId.POW26 = Math.pow(2, 26); // 2 ^ 26
 
-			// If all sequence values (4096 unique values including 0) have been used 
-			// to generate ids in the current millisecond (overflow is true) wait till next millisecond 
-			if(this.overflow) {
-				if(cb) {
-					setTimeout(this.next.bind(this, cb), 1);
-					return;
-				}
-				else throw new Error('Sequence exceeded its maximum value. Provide callback function to handle sequence overflow');
-			}
+    FlakeId.prototype = {
+        next: function (cb) {
+            var id = new Buffer(8), time = Date.now() - this.epoch;
+            id.fill(0);
 
-			// Increase sequence counter
-			this.seq = (this.seq + 1) & 0xFFF;
+            // Generates id in the same millisecond as the previous id
+            if (time === this.lastTime) {
 
-			// sequence counter exceeded its max value (4095) 
-			// - set overflow flag and wait till next millisecond
-			if(this.seq === 0) {
-				this.overflow = true;
-				if(cb) {
-					setTimeout(this.next.bind(this, cb), 1);
-					return;
-				}
-				else throw new Error('Sequence exceeded its maximum value. Provide callback function to handle sequence overflow');
-			}
-		} else {
-			this.overflow = false;
-			this.seq = 0;
-		}
-		this.lastTime = time;
+                // If all sequence values (4096 unique values including 0) have been used
+                // to generate ids in the current millisecond (overflow is true) wait till next millisecond
+                if (this.overflow) {
+                    if (cb) {
+                        setTimeout(this.next.bind(this, cb), 1);
+                        return;
+                    }
+                    else {
+                        throw new Error('Sequence exceeded its maximum value. Provide callback function to handle sequence overflow');
+                    }
+                }
 
-		id.writeUInt32BE(((time & 0x3) << 22) | this.id | this.seq, 4);
-		id.writeUInt8(Math.floor(time / 4) & 0xFF, 4);
-		id.writeUInt16BE(Math.floor(time / FlakeId.POW10) & 0xFFFF, 2)
-		id.writeUInt16BE(Math.floor(time / FlakeId.POW26) & 0xFFFF, 0)
+                // Increase sequence counter
+                this.seq = (this.seq + 1) & 0xFFF;
 
-		if(cb) process.nextTick(cb.bind(null, null, id));
-		else return id;
-	}
-};
+                // sequence counter exceeded its max value (4095)
+                // - set overflow flag and wait till next millisecond
+                if (this.seq === 0) {
+                    this.overflow = true;
+                    if (cb) {
+                        setTimeout(this.next.bind(this, cb), 1);
+                        return;
+                    }
+                    else {
+                        throw new Error('Sequence exceeded its maximum value. Provide callback function to handle sequence overflow');
+                    }
+                }
+            } else {
+                this.overflow = false;
+                this.seq = 0;
+            }
+            this.lastTime = time;
+
+            id.writeUInt32BE(((time & 0x3) << 22) | this.id | this.seq, 4);
+            id.writeUInt8(Math.floor(time / 4) & 0xFF, 4);
+            id.writeUInt16BE(Math.floor(time / FlakeId.POW10) & 0xFFFF, 2);
+            id.writeUInt16BE(Math.floor(time / FlakeId.POW26) & 0xFFFF, 0);
+
+            if (cb) {
+                process.nextTick(cb.bind(null, null, id));
+            }
+            else {
+                return id;
+            }
+        }
+    };
+
+}());
